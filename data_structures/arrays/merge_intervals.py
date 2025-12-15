565edde82ec05f64e7d5f8a55ec966025839e298
Gokul Krishnan <gokulkri247@gmail.com>
2025-12-14 22:07:00 -0800

    added merging_interval

---
+++ data_structures/arrays/merge_intervals.py
@@ -0,0 +1,51 @@
+from typing import List
+
+
+def merge_intervals(intervals: List[List[int]]) -> List[List[int]]:
+    """
+    Merges all overlapping intervals.
+
+    Given a list of intervals where each interval is represented as
+    [start, end], this function merges all overlapping intervals and
+    returns a list of non-overlapping intervals sorted by start time.
+
+    Parameters:
+    intervals (List[List[int]]): A list of intervals.
+
+    Returns:
+    List[List[int]]: A list of merged non-overlapping intervals.
+
+    Examples:
+    >>> merge_intervals([[1, 3], [2, 6], [8, 10], [15, 18]])
+    [[1, 6], [8, 10], [15, 18]]
+    >>> merge_intervals([[1, 4], [4, 5]])
+    [[1, 5]]
+    >>> merge_intervals([])
+    []
+    >>> merge_intervals([[1, 4]])
+    [[1, 4]]
+
+    Time Complexity:
+    O(n log n), where n is the number of intervals.
+
+    Space Complexity:
+    O(n), for storing the merged intervals.
+    """
+
+    if not intervals:
+        return []
+
+    # Sort intervals by starting time
+    intervals.sort(key=lambda x: x[0])
+
+    merged: List[List[int]] = [intervals[0]]
+
+    for current in intervals[1:]:
+        last = merged[-1]
+
+        if current[0] <= last[1]:
+            last[1] = max(last[1], current[1])
+        else:
+            merged.append(current)
+
+    return merged
